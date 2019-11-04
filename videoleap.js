4cafb1cd3e973e173416fa0a4917db2d587a16c9
travisoa <37182043+travisoa@users.noreply.github.com>
2019-11-05 03:54:44 +0800

    Update and rename quan to videoleap.js

---
+++ videoleap.js
@@ -0,0 +1,15 @@
+{
+ "fullRefundDateMs": null,
+ "isExpired": false,
+ "latestExpirationDateMs": 1587631226000,
+ "latestProductId": "com.lightricks.EnlightVideo_V2.PA.1Y.SA_1Y.SA_TRIAL.3D",
+ "latestPurchaseDateMs": 1556008826000,
+ "latestTransactionId": "410000554943691",
+ "originalPurchaseDateMs": 1555565892000,
+ "originalTransactionId": "410000511799185",
+ "pendingRenewalInfo": {
+  "expirationIntent": null,
+  "isAutoRenewEnabled": false,
+  "nextProductId": "com.lightricks.EnlightVideo_V2.PA.1Y.SA_1Y.SA_TRIAL.3D"
+ }
+}
